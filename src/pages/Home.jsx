81b22cf755d858360142fbc4d0000957e6ce6cf1
nicolo_canevari <nico.canevari@outlook.it>
2025-02-24 16:19:12 +0100

    [FEAT] esportazione del layout, home , about

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+
+        <div>
+
+            <h1>Benvenuto nella Homepage!</h1>
+            <p>Questa è la Home principale del nostro sito.</p>
+
+        </div>
+
+    );
+
+}
+
+export default Home; 
