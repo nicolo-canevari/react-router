be1adb156c5866971bd8acb299fe88712c11aeaf
nicolo_canevari <nico.canevari@outlook.it>
2025-02-24 15:44:08 +0100

    [FEAT] install react-router-dom, importo i nuovi file su app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// layout che include il navbar
+import Layout from "../src/layouts/Layout";
+// pagina home
+import Home from "../src/pages/Home";
+// pagina chi siamo
+import About from "../src/pages/About";
 import ArticleList from '../src/components/ArticleList';
 import './App.css'
 
@@ -5,12 +12,28 @@ const App = () => {
 
   return (
 
-    <div>
+    <Router>
 
-      <h1>Lista degli Articoli</h1>
-      <ArticleList />
+      <Layout>
 
-    </div>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<ArticleList />} />
+
+        </Routes>
+
+      </Layout>
+
+    </Router>
+
+    // <div>
+
+    //   <h1>Lista degli Articoli</h1>
+    //   <ArticleList />
+
+    // </div>
 
   );
 
